caef566a3cf44723849556f25cf2d4c2be567d94
jgrabski <jedrzej@grabski.pl>
2023-10-24 17:31:24 +0200

    Added global functions.

--- src/giga.pl
+++ src/giga.pl
@@ -59,6 +59,51 @@ notice_objects_at(Place) :-
 
 notice_objects_at(_).
 
+check_inventory() :-
+    holding(X),
+    write('You have one '), write(X), write(' in your bag.'), nl,
+    fail.
+
+check_inventory().
+
+
+/* This rule tells how to die. */
+
+die :-
+    finish.
+
+
+finish :-
+    nl,
+    write('The game is over. Please enter the "halt." command.'),
+    nl.
+
+/* This rule just writes out game instructions. */
+
+instructions :-
+    nl,
+    write('Enter commands using standard Prolog syntax.'), nl,
+    write('Available commands are:'), nl,
+    write('start.             -- to start the game.'), nl,
+    write('n.  s.  e.  w.     -- to go in that direction.'), nl,
+    write('take(Object).      -- to pick up an object.'), nl,
+    write('drop(Object).      -- to put down an object.'), nl,
+    write('use_on(Object1, Object2). -- to use an Object1 on Object2.'), nl,
+    write('check_inventory.   -- to list the objects you are holding.'), nl,
+    write('inspect(Object).   -- to inspect an object.'), nl,	
+    write('look.              -- to look around you again.'), nl,
+    write('instructions.      -- to see this message again.'), nl,
+    write('halt.              -- to end the game and quit.'), nl,
+    nl.
+
+start :-
+    instructions,
+    look.
+
+
+
+
+
 % ===============================================
 
 
