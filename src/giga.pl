0fe859984750ea23efb547aaa76bc4d9160f0516
jgrabski <jedrzej@grabski.pl>
2023-10-25 10:00:36 +0200

    Finished crew_bedroom room. Player introduction to craft, use, open, inspect mechanics.

--- src/giga.pl
+++ src/giga.pl
@@ -1,8 +1,10 @@
 % ============= funkcjonalne=====================
-:- (dynamic i_am_at/1, at/2, have/1, path/3, pickable/1).
+:- (dynamic i_am_at/1, at/2, have/1, path/3, pickable/1, locked/1).
 :- retractall(at(_, _)),
    retractall(i_am_at(_)),
    retractall(alive(_)).
+:- discontiguous i_am_at/1, at/2, have/1, path/3, pickable/1, describe/1,
+    inspectable/1, inspect/1, locked/1, open/1, use/2.
 
 take(X) :-
     have(X),
@@ -77,22 +79,17 @@ go(_) :-
     write("You can't go that way.").
 
 % look logic
-look :-
+look:-
     i_am_at(Place),
     describe(Place),
     nl,
     notice_objects_at(Place).
 
-% describing places
-
-describe(bridge) :-
-    write("you are the bridge."), !.
-
 
 % notice objects at places
 notice_objects_at(Place) :-
     at(X, Place),
-    write("There is *"),
+    write("There is a/an *"),
     write(X),
     write("* here."),
     nl,
@@ -102,7 +99,7 @@ notice_objects_at(_).
 
 check_inventory() :-
     have(X),
-    write('You have one '), write(X), write(' in your bag.'), nl,
+    write('You have one *'), write(X), write('* in your bag.'), nl,
     fail.
 
 check_inventory().
@@ -129,7 +126,8 @@ instructions :-
     write('n.  s.  e.  w.     -- to go in that direction.'), nl,
     write('take(Object).      -- to pick up an object.'), nl,
     write('drop(Object).      -- to put down an object.'), nl,
-    write('use_on(Object1, Object2). -- to use an Object1 on Object2.'), nl,
+    write('use(Object1, Object2). -- to use an Object1 on Object2.'), nl,
+    write('open(Object).      -- to open Object if it''s possible.'), nl,
     write('check_inventory.   -- to list the objects you are holding.'), nl,
     write('inspect(Object).   -- to inspect an object.'), nl,
     write('look.              -- to look around you again.'), nl,
@@ -154,25 +152,112 @@ start :-
 % =================jedrek===================================
 % My location
 i_am_at(crew_bedroom).
+
+% Descriptions
 describe(crew_bedroom):-
-    write("A loud crashing sound wakes you up in you bed inside the engineering crew bedroom. You look around and see that the room is in a mess. A vent in the east corner of the room swings wide open. Blaring alarms can be heard from the corridor outside. You see a locker, a desk and a bed. You need to act, fast."), nl, nl,
+    nl, write("A loud crashing sound wakes you up in you bed inside the engineering crew bedroom. You look around and see that the room is in a mess. A vent in the east corner of the room swings wide open. Blaring alarms can be heard from the main corridor on the south side. You see a locker, a desk and a bed. You need to act, fast."), nl, nl,
     write("You are at the crew bedroom."), nl, !, nl.
 
+describe(crew_bedroom_vent):-
+    nl, write("You crawl into a rather spatious crew bedroom vent. There it is! This is where you *desk_key* went! Good thing it didn\'t fall deeper or you would be stuck in here for ever."), nl, !, nl.
+
 % Paths
-path(main_corridor, n, crew_bedroom).
-path(crew_bedroom, s, main_corridor).
+% crew_bedroom n<=>s main_corridor
+% has to be unlocked
+
+% crew_bedroom w<=>e crew_bedroom_vent
 path(crew_bedroom, e, crew_bedroom_vent).
 path(crew_bedroom_vent, w, crew_bedroom).
 
-% Object at crew_bedroom
-at(crew_bedroom, bed).
-at(crew_bedroom, desk).
-at(crew_bedroom, locker).
 
-% Pickable objects at crew_bedroom
-at(desk_key, crew_bedroom).
+
+% Crafting
+craft(hammer_head, hammer_handle):-
+    have(hammer_head),
+    have(hammer_handle),
+    retract(have(hammer_head)),
+    retract(have(hammer_handle)),
+    assert(have(hammer)),
+    nl, write("Those two came togheter easily! Now you\'ve got a full *hammer*!"), nl, !, nl.
+
+
+
+% Crew Bedroom ================================================================
+
+% Objects at crew_bedroom
+at(bed, crew_bedroom).
+at(desk, crew_bedroom).
+locked(desk).
+at(locker, crew_bedroom).
+
+at(security_door, crew_bedroom).
+locked(security_door).
+
+% Use cases - Crew Bedroom
+use(crew_access_card, security_door):-
+    have(crew_access_card),
+    locked(security_door),
+    retract(locked(security_door)),
+    nl, write("You swipe your crew access card on the security door and hear the metal latch inside loudly open. Good thing the electronics on the ship still work."), nl, !, nl.
+
+use(crew_access_card, security_door):-
+    have(crew_access_card),
+    nl, write("The security door is already unlocked."), nl, !, nl.
+
+open(security_door):-
+    locked(security_door),
+    nl, write("The security door is locked. It won\'t budge."), nl, !, nl.
+
+open(security_door):-
+    nl, write("The security door, now unlocked, opens smoothly. In front of you you see the main corridor shrouded in smoke."), nl,
+    assert(path(crew_bedroom, s, main_corridor)),
+    assert(path(main_corridor, n, crew_bedroom)),
+    !, nl.
+
+use(desk_key, desk):-
+    have(desk_key),
+    locked(desk),
+    retract(locked(desk)),
+    retract(at(desk, crew_bedroom)),
+    assert(at(crew_access_card, crew_bedroom)),
+    assert(pickable(crew_access_card)),
+    nl, write("You unlock the desk with the key you found in the vent. Inside you see your *crew_access_card*."), nl, !, nl.
+
+
+% Inspects - Crew Bedroom
+inspect(bed):-
+    nl, write("There is nothing special on this bed, aside from a *thick_blanket*. I wonder if it could be useful later."), nl,
+    assert(at(thick_blanket, crew_bedroom)),
+    assert(pickable(thick_blanket)),
+    retract(at(bed, crew_bedroom)),
+    !, nl.
+
+inspect(desk):-
+    locked(desk),
+    nl, write("The desk is locked. There should be a key on it, but maybe it flew somewhere during the crash."), nl, !, nl.
+
+inspect(locker):-
+    nl, write("You look inside your locked hoping to find some of your tools. "),
+    nl, write("Unfortunately, all of the electronic tools got damaged. However your trusty hammer only came apart. I bet you could stick the *hammer_head* and the *hammer_handle* togheter and fix it up."),
+    assert(at(hammer_head, crew_bedroom)),
+    assert(at(hammer_handle, crew_bedroom)),
+    assert(pickable(hammer_head)),
+    assert(pickable(hammer_handle)),
+    retract(at(locker, crew_bedroom)),
+    !, nl.
+
+inspect(security_door):-
+    locked(security_door),
+    nl, write("The door leading out of your room to the main corridor remains locked. You need your own *crew_access_card* to unlock it."), nl, !, nl.
+
+inspect(security_door):-
+    nl, write("The door leading out of your room to the main corridor is unlocked. You can now go to the main corridor."), nl, !, nl.
+
+% ====================================================
+
+% Crew Bedroom Vent
+at(desk_key, crew_bedroom_vent).
 pickable(desk_key).
-
 
 
 
@@ -185,11 +270,14 @@ pickable(desk_key).
 
 
 % =================olek===================================
-i_am_at(bridge).
+%i_am_at(bridge).
 path(bridge, n, void).
-at(bridge, vent).
+at(bridge, vent). % at(thing, place) btw
 inspectable(vent).
 
+
+describe(bridge) :-
+    write("You are at the bridge. You see a vent in the north corner of the room."), nl, !, nl.
 
 inspect(vent) :-
     assert(i_am_at(vent)),
